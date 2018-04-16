66c9b53ef27f4f56314ebd2c2e6e0033957d6fe6
Jameer Basha <jameerbasha14@gmail.com>
2018-04-16 19:56:19 +0530

    Revision
    Errors in the code are handled

--- priority-scheduling.c
+++ priority-scheduling.c
@@ -1,17 +1,31 @@
 #include<stdio.h>
-#include<unistd.h>
-#include<stdlib.h>
-#include<pthread.h>
 int main()
 {
-	int num,i;
+	printf("\n****Priority Calculator****\n");
+	unsigned int num,i,choice,errorflag=0;
 	printf("Enter the Number of Processes: ");
 	scanf("%d",&num);
-	int recent[num],base=60,priority[num];
+	if(num>100||num<1)
+	{
+		printf("\nWrong input Enter the correct input again");
+		errorflag=1;
+		main();
+	}
+	unsigned int recent[num],base=60,priority[num];
 	for(i=0;i<num;i++)
 	{
 		printf("Enter the Recent CPU Usage of process %d : ",i+1);
 		scanf("%d",&recent[i]);
+		if(recent[i]>1000||recent[i]<0)
+		{
+			printf("\nWrong input Enter the correct input again");
+			errorflag=1;
+			break;
+		}
+	}
+	if(errorflag==1)
+	{
+		main();
 	}
 	for(i=0;i<num;i++)
 	{
@@ -22,4 +36,8 @@ int main()
 	{
 		printf("\n\t%d\t%d",i+1,priority[i]);
 	}
+	printf("\nEnter 1 to calculate again or anything else to exit:");
+	scanf("%d",&choice);
+	if(choice==1)
+	main();
 }
